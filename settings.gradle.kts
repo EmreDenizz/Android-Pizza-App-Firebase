d09f9ef05b29996119f7c2eb3af7b5b1d70d490d
Emre Deniz <emre.deniz@outlook.com>
2023-11-18 09:05:20 -0500

    Initial commit

---
+++ settings.gradle.kts
@@ -0,0 +1,18 @@
+pluginManagement {
+    repositories {
+        google()
+        mavenCentral()
+        gradlePluginPortal()
+    }
+}
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    repositories {
+        google()
+        mavenCentral()
+    }
+}
+
+rootProject.name = "Group9_MAPD711_Project"
+include(":app")
+ 
